bf311ca06230d168971c2b1a01d281a5ec9cd317
Tiago Silveira De Bittencourt <tiago.40123@alunosatc.edu.br>
2025-11-03 19:29:50 -0300

    focus

--- src/scenarios/NextFit.spec.ts
+++ src/scenarios/NextFit.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { join } from 'path';
 import { TheConfig } from 'sicolo';
 import NextFitPage from '../support/pages/NextFitPage';
@@ -19,5 +19,26 @@ test.describe('Testes funcionais no site da NextFit', () => {
     await nextFitPage.preencherFormulario();
     await nextFitPage.enviarFormulario();
     await nextFitPage.validarEnvio();
+  });
+
+  test('Preencher somente e-mail deve deixar o campo de nome com foco no cursor', async ({ page }) => {
+    const nextFitElements = new NextFitPage(page).nextFitElements;
+  
+    await nextFitElements.getCampoEmail().waitFor({ state: 'visible' });
+  
+    const email = 'teste.focus@exemplo.com';
+    await nextFitElements.getCampoEmail().fill(email);
+  
+    await nextFitElements.getCampoEmail().press('Tab');
+  
+    await nextFitElements.getBotaoEnviar().click({ force: true });
+  
+    await page.waitForTimeout(500);
+  
+    const focusedElement = await page.evaluate(() => document.activeElement?.getAttribute('name'));
+  
+    expect(focusedElement).toBe('form_fields[name]');
+  
+    console.log('Elemento em foco após envio inválido:', focusedElement);
   });
 });
